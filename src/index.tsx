398ec52c313fa5e5bce65fcf0382772b2c2bf448
Bryce Meyer <bjm904@gmail.com>
2022-06-11 15:01:20 -0700

    Index: Remove debug code

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { when } from 'mobx';
 import App from './App';
-import Canidate from './store/Canidate';
 import store from './store';
 
 // I know you'll see this but for the love of god, dont block the initial render
 await when(() => store.state.ready);
-
-// Test state persistence
-store.addCanidate(new Canidate({ title: Date.now().toString() }));
 
 // root element will always be there. I am comfortable with the assertion in this case
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
