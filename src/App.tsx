9b85b797dc403b09e969b9a042e2967f5583b6f3
Bryce Meyer <bjm904@gmail.com>
2022-06-11 15:03:49 -0700

    App: Add "Reset State" button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React, { ReactElement } from 'react';
+import store from './store';
+
+const onClick = (): void => store.init();
 
 function App(): ReactElement {
   return (
     <div>
       Im reacting
+      <input type="button" onClick={onClick} value="Reset State" />
     </div>
   );
 }
