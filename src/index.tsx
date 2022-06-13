96653b9dab865f6b673153a16854479226f851f5
Bryce Meyer <bjm904@gmail.com>
2022-06-13 13:42:39 -0700

    Index: No longer block initial render waiting on state ready

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { when } from 'mobx';
 import App from './App';
 import store, { StateContext } from './store';
-
-// I know you'll see this but for the love of god, dont block the initial render
-await when(() => store.state.ready);
 
 // root element will always be there. I am comfortable with the assertion in this case
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
